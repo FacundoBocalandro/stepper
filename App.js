ef97fb2d11beb8f483bab727a761a92ef38f5a9a
Facundo Bocalandro <facundo@bocalandro.com.ar>
2020-07-21 11:51:49 -0300

    first and second step merged

--- App.js
+++ App.js
@@ -1,23 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ImageUpload from "./second_step/ImageUpload";
-
-
+import {ProgressSteps, ProgressStep} from 'react-native-progress-steps';
+import FirstStep from "./first-step/FirstStep";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <ImageUpload/>
-    </View>
-  );
+
+    const [firstStep, setFirstStep] = React.useState({
+        email: '',
+        username: '',
+        password: '',
+        repeat: '',
+        isValid: false,
+        error: {
+            hasError: false,
+        },
+    })
+
+    const validateEmail = (mail) => {
+        return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(mail);
+    }
+
+    const checkFirstStepValidity = () => {
+        const checkEmail = validateEmail(firstStep.email);
+        const checkUsername = firstStep.username.length >= 4;
+        const checkPass = firstStep.repeat === firstStep.password;
+        if (checkEmail && checkUsername && checkPass){
+            setFirstStep({
+                ...firstStep,
+                isValid: true,
+                error:
+                    {
+                        hasError: false,
+                    },
+            })
+        }else {
+            setFirstStep({
+                ...firstStep,
+                isValid: false,
+                error: {
+                    hasError: true,
+                    emailError: !checkEmail,
+                    usernameError: !checkUsername,
+                    passwordError: !checkPass,
+                },
+            })
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <ProgressSteps>
+                <ProgressStep label="First Step" onNext={checkFirstStepValidity} errors={firstStep.error.hasError}>
+                    <View>
+                        <FirstStep firstStep={firstStep} setFirstStep={setFirstStep}/>
+                    </View>
+                </ProgressStep>
+                <ProgressStep label="Second Step">
+                    <View>
+                      <ImageUpload/>
+                    </View>
+                </ProgressStep>
+                <ProgressStep label="Third Step">
+                    <View style={{alignItems: 'center'}}>
+                        <Text>This is the content within step 3!</Text>
+                    </View>
+                </ProgressStep>
+            </ProgressSteps>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        top: 10,
+    },
 });
---
+++ first-step/FirstStep.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import {Form, Item, Input, Text} from 'native-base';
+
+export default function FirstStep(props) {
+
+    return (
+        <Form>
+            <Item>
+                <Input placeholder="Email"
+                       autoCompleteType={'email'}
+                       textContentType={'emailAddress'}
+                       value={props.firstStep.email}
+                       onChangeText={text => props.setFirstStep({
+                           ...props.firstStep,
+                           email: text,
+                       })}
+                />
+            </Item>
+            {props.firstStep.error.emailError?
+                <Text style={{color: 'red'}}>Invalid Email!</Text>
+                :
+                <></>
+            }
+            <Item>
+                <Input placeholder="Username"
+                       autoCompleteType={'username'}
+                       maxLength={15}
+                       textContentType={'username'}
+                       value={props.firstStep.username}
+                       onChangeText={text => props.setFirstStep({
+                           ...props.firstStep,
+                           username: text,
+                       })}
+                />
+            </Item>
+            {props.firstStep.error.usernameError?
+                <Text style={{color: 'red'}}>Username should have at least 4 characters!</Text>
+                :
+                <></>
+            }
+            <Item>
+                <Input placeholder="Password"
+                       secureTextEntry
+                       autoCompleteType={'password'}
+                       textContentType={'password'}
+                       value={props.firstStep.password}
+                       onChangeText={text => props.setFirstStep({
+                           ...props.firstStep,
+                           password: text,
+                       })}
+                />
+            </Item>
+            <Item>
+                <Input placeholder="Repeat Password"
+                       secureTextEntry
+                       autoCompleteType={'password'}
+                       textContentType={'password'}
+                       value={props.firstStep.repeat}
+                       onChangeText={text => props.setFirstStep({
+                           ...props.firstStep,
+                           repeat: text,
+                       })}
+                />
+            </Item>
+            {props.firstStep.error.passwordError?
+                <Text style={{color: 'red'}}>Password not matching!</Text>
+                :
+                <></>
+            }
+        </Form>
+    );
+}
