9851eb622b254f7f6e2d4b524196848f7e60512b
BraianBiale <braianbiale@gmail.com>
2020-07-21 11:40:16 -0300

    ImageUpload Done

---
+++ second_step/ImageUpload.js
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from "react";
+import {View, Button, Image} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+import Constants from "expo-constants";
+
+const ImageUpload = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    useEffect(() => {
+        getPermissionAsync();
+    });
+
+    const getPermissionAsync = async () => {
+        if (Constants.platform.ios) {
+            const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+            if (status !== 'granted') {
+                alert('Sorry, we need camera roll permissions to make this work!');
+            }
+        }
+    };
+
+    const _pickImage = async () => {
+        try {
+            let result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.All,
+                allowsEditing: true,
+                aspect: [4, 3],
+                quality: 1,
+            });
+            if (!result.cancelled) {
+                setSelectedImage({ image: result.uri });
+            }
+
+            console.log(result);
+        } catch (E) {
+            console.log(E);
+        }
+    }
+
+    return (
+        <View>
+            {selectedImage && <Image source={{uri: selectedImage.image}} style={{width: 200, height: 200, marginBottom: 10}}/>}
+            <Button onPress={() => _pickImage()} title={"Select Image"}/>
+        </View>
+    );
+}
+
+export default ImageUpload
